49c7d315ddbe60f4c78fca6f62e0ff9134b36630
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2018-04-23 18:08:11 -0700

    Added ranking function

--- evolver.go
+++ evolver.go
@@ -1,20 +1,69 @@
 package main
 
 import (
+	"fmt"
+	"image"
 	"image/color"
+	"log"
 	"math/rand"
+	"os"
 	"time"
 
+	"github.com/fogleman/gg"
+
 	"bitbucket.org/wolfgang_meyers/evolve.paint.digital/instructions"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	app        = kingpin.New("evolver", "Program to evolve paintings from a reference image")
+	testCmd    = app.Command("test", "A test command to develop features of the evolver")
+	targetFile = testCmd.Arg("target", "File containing the target image").Required().String()
+
+	compareCmd   = app.Command("compare", "Compares two image files for difference and prints the result")
+	compareFile1 = compareCmd.Arg("file1", "First file to compare").Required().String()
+	compareFile2 = compareCmd.Arg("file2", "Second file to compare").Required().String()
 )
 
 func init() {
 	rand.Seed(time.Now().Unix())
 }
 
+func loadImage(imageFile string) image.Image {
+	img, err := gg.LoadImage(imageFile)
+	if err != nil {
+		log.Fatalf("Error loading target image from file '%v': %v", imageFile, err.Error())
+	}
+	return img
+}
+
 func main() {
+	cmd := kingpin.MustParse(app.Parse(os.Args[1:]))
+	switch cmd {
+	case testCmd.FullCommand():
+		test()
+	case compareCmd.FullCommand():
+		compare()
+	default:
+		log.Fatalf("Unimplemented command: %v", cmd)
+	}
+}
+
+func compare() {
+	image1 := loadImage(*compareFile1)
+	image2 := loadImage(*compareFile2)
+	ranker := &Ranker{}
+	diff, err := ranker.Distance(image1, image2)
+	if err != nil {
+		log.Fatalf("Error comparing images: %v", err.Error())
+	}
+	fmt.Printf("Diff: %v", diff)
+}
+
+func test() {
+	target := loadImage(*targetFile)
 	numlines := 300
-	renderer := NewRenderer(1000, 1000)
+	renderer := NewRenderer(target.Bounds().Size().X, target.Bounds().Size().Y)
 	items := make([]Instruction, numlines)
 	for i := 0; i < numlines; i++ {
 		line := &instructions.Line{
@@ -33,5 +82,14 @@ func main() {
 		items[i] = line
 	}
 	renderer.Render(items)
+	rendered := renderer.GetImage()
+
+	ranker := &Ranker{}
+	diff, err := ranker.Distance(target, rendered)
+	if err != nil {
+		log.Fatalf("Error calculating distance: %v", err.Error())
+	}
+	log.Printf("Difference: %v", diff)
+
 	renderer.SaveToFile("test.png")
 }
---
+++ ranker.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"math"
+
+	"github.com/lucasb-eyer/go-colorful"
+)
+
+// Lab represents a color in the Lab color space.
+// See https://en.wikipedia.org/wiki/Lab_color_space#CIELAB
+type Lab struct {
+	l float64
+	a float64
+	b float64
+}
+
+// NewLab returns a new `Lab` from its components
+func NewLab(l float64, a float64, b float64) *Lab {
+	return &Lab{
+		l: l,
+		a: a,
+		b: b,
+	}
+}
+
+// A Ranker calculates the difference between two images
+// by comparing their pixel colors in the Lab color space
+type Ranker struct {
+}
+
+// Distance calculates the distance between two images by comparing each pixel
+// between the images in the Lab color space.
+func (ranker *Ranker) Distance(image1 image.Image, image2 image.Image) (float64, error) {
+	if image1.Bounds().Size().X != image2.Bounds().Size().X || image1.Bounds().Size().Y != image2.Bounds().Size().Y {
+		return 0, fmt.Errorf("Images are not the same size")
+	}
+	var diff float64
+	var count float64
+	for x := 0; x < image1.Bounds().Size().X; x++ {
+		for y := 0; y < image1.Bounds().Size().Y; y++ {
+			color1 := image1.At(x, y)
+			color2 := image2.At(x, y)
+			diff += ranker.colorDistance(color1, color2)
+			count++
+		}
+	}
+	return diff / count, nil
+}
+
+// Calculates the distance between two colors using the Lab color space
+func (ranker *Ranker) colorDistance(color1 color.Color, color2 color.Color) float64 {
+	lab1 := NewLab(ranker.makeColor(color1).Lab())
+	lab2 := NewLab(ranker.makeColor(color2).Lab())
+	lDiff := math.Pow(lab2.l-lab1.l, 2)
+	aDiff := math.Pow(lab2.a-lab1.a, 2)
+	bDiff := math.Pow(lab2.b-lab1.b, 2)
+	return math.Sqrt(lDiff + aDiff + bDiff)
+}
+
+func (ranker *Ranker) makeColor(clr color.Color) colorful.Color {
+	r, g, b, _ := clr.RGBA()
+	result := colorful.Color{
+		R: float64(r) / 255.0,
+		G: float64(g) / 255.0,
+		B: float64(b) / 255.0,
+	}
+	return result
+}
