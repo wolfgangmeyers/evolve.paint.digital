050350177ea51cfc067b7c9532c7244de29f0d45
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2018-09-21 22:56:45 -0700

    Implemented focus editor/shaders

--- evolver-webgl/js/focus.js
+++ evolver-webgl/js/focus.js
@@ -1,4 +1,4 @@
-
+// A glorified gpu-based painting application.
 function FocusEditor(gl, focusMapProgram, displayProgram, srcImage, focusMap) {
     this.gl = gl;
     this.focusMapProgram = focusMapProgram;
@@ -9,39 +9,178 @@ function FocusEditor(gl, focusMapProgram, displayProgram, srcImage, focusMap) {
         var height = (srcImage.height / srcImage.width) * width;
         focusMap = new FocusMap(width, height);
     }
+    this.focusMap = focusMap;
     this.mouseDown = 0;
     this.mousePosition = {
         x: 0,
         y: 0,
     };
+    this.brushFocus = 1;
+    this.brushSize = 0.01;
+    this.focusOpacity = 0.5;
 
     // Set up webgl stuff
     gl.useProgram(this.focusMapProgram);
     // Get attribute/uniform locations
-    this.focusPosLocation = gl.getAttributeLocation(this.focusMapProgram, "a_position");
-    this.focusTexCoordLocation = gl.getAttributeLocation(this.focusMapProgram, "a_texCoord");
-    this.focusMapLocation = gl.getAttributeLocation(this.focusMapProgram, "u_focus");
-    this.focusMouseDownLocation = gl.getAttributeLocation(this.focusMapProgram, "u_mouseDown");
-    this.focusMousePosLocation = gl.getAttributeLocation(this.focusMapLocation, "u_mousePos");
-    
-    this.displayPosLocation = gl.getAttributeLocation(this.displayProgram, "a_position");
-    this.displayTexCoordLocation = gl.getAttributeLocation(this.displayProgram, "a_texCoord");
-    this.displaySrcLocation = gl.getAttributeLocation(this.displayProgram, "u_src");
-    this.displayFocusLocation = gl.getAttributeLocation(this.displayProgram, "u_focus");
-    this.displayMousePosLocation = gl.getAttributeLocation(this.displayProgram, "u_mousePos");
+    this.focusPosLocation = gl.getAttribLocation(this.focusMapProgram, "a_position");
+    this.focusTexCoordLocation = gl.getAttribLocation(this.focusMapProgram, "a_texCoord");
+    this.focusMapLocation = gl.getUniformLocation(this.focusMapProgram, "u_focus");
+    this.focusMouseDownLocation = gl.getUniformLocation(this.focusMapProgram, "u_mouseDown");
+    this.focusMousePosLocation = gl.getUniformLocation(this.focusMapProgram, "u_mousePos");
+    this.focusBrushLocation = gl.getUniformLocation(this.focusMapProgram, "u_brushFocus");
+    this.focusBrushSizeLocation = gl.getUniformLocation(this.focusMapProgram, "u_brushSize");
+
+    this.displayPosLocation = gl.getAttribLocation(this.displayProgram, "a_position");
+    this.displayTexCoordLocation = gl.getAttribLocation(this.displayProgram, "a_texCoord");
+    this.displaySrcLocation = gl.getUniformLocation(this.displayProgram, "u_src");
+    this.displayFocusLocation = gl.getUniformLocation(this.displayProgram, "u_focus");
+    this.displayMousePosLocation = gl.getUniformLocation(this.displayProgram, "u_mousePos");
+    this.displayBrushSizeLocation = gl.getUniformLocation(this.displayProgram, "u_brushSize");
+    this.displayFocusOpacityLocation = gl.getUniformLocation(this.displayProgram, "u_focusOpacity");
 
     // Create attribute buffers
     this.posBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, this.posBuffer);
     setRectangle(gl, -1, -1, 2, 2, {dynamic: true});
-    this.texCoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.texCoordBuffer);
+    this.focusTexcoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.focusTexcoordBuffer);
+    setRectangle(gl, 0, 0, 1, 1, {flipY: false, dynamic: true});
+    this.displayTexcoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.displayTexcoordBuffer);
     setRectangle(gl, 0, 0, 1, 1, {flipY: true, dynamic: true});
 
     // Create textures
+    this.focusTextures = [
+        this.createFocusTexture(this.focusMap.imageData),
+        this.createFocusTexture(this.focusMap.imageData),
+    ];
+    this.srcTexture = createAndSetupTexture(gl, 0);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.srcImage);
 
     // Create framebuffers
+    this.framebuffers = [
+        this.createFramebuffer(this.focusTextures[0]),
+        this.createFramebuffer(this.focusTextures[1]),
+    ];
+    // Phase indicates which framebuffer to use - 0 or 1
+    this.phase = 0;
+}
 
+FocusEditor.prototype.setMousePosition = function(x, y) {
+    this.mousePosition.x = x;
+    this.mousePosition.y = y;
+}
+
+FocusEditor.prototype.setMouseDown = function(mouseDown) {
+    this.mouseDown = mouseDown;
+}
+
+FocusEditor.prototype.setBrushFocus = function(brush) {
+    this.brushFocus = brush;
+}
+
+FocusEditor.prototype.setBrushSize = function(brushSize) {
+    this.brushSize = brushSize;
+}
+
+FocusEditor.prototype.render = function() {
+    var gl = this.gl;
+    // First, render focusmap
+    // gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.viewport(0, 0, this.focusMap.width, this.focusMap.height);
+    gl.useProgram(this.focusMapProgram);
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.uniform1i(this.focusMapLocation, 0);
+    gl.uniform1i(this.focusMouseDownLocation, this.mouseDown);
+    gl.uniform2f(this.focusMousePosLocation, this.mousePosition.x, this.mousePosition.y);
+    gl.uniform1f(this.focusBrushLocation, this.brushFocus);
+    gl.uniform1f(this.focusBrushSizeLocation, this.brushSize);
+
+    // Set up attributes for focus map render
+    gl.enableVertexAttribArray(this.focusPosLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.posBuffer);
+    gl.vertexAttribPointer(this.focusPosLocation, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(this.focusTexCoordLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.focusTexcoordBuffer);
+    gl.vertexAttribPointer(this.focusTexCoordLocation, 2, gl.FLOAT, false, 0, 0);
+
+    gl.activeTexture(gl.TEXTURE0);
+    if (this.phase == 0) {
+        gl.bindTexture(gl.TEXTURE_2D, this.focusTextures[0]);
+        gl.activeTexture(gl.TEXTURE1);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebuffers[1]);
+    } else {
+        gl.bindTexture(gl.TEXTURE_2D, this.focusTextures[1]);
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebuffers[0]);
+    }
+    // draw
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 6;
+    gl.drawArrays(primitiveType, offset, count);
+
+    // Free framebuffer
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+    // Second, render focusmap + src image + brush position
+    gl.useProgram(this.displayProgram);
+    
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    // gl.viewport(0, 0, this.srcImage.width, this.srcImage.height);
+    gl.uniform1i(this.displayFocusLocation, 0);
+    gl.uniform1i(this.displaySrcLocation, 1);
+    gl.uniform2f(this.displayMousePosLocation, this.mousePosition.x, this.mousePosition.y);
+    gl.uniform1f(this.displayBrushSizeLocation, this.brushSize);
+    gl.uniform1f(this.displayFocusOpacityLocation, this.focusOpacity);
+
+    // Set up attributes for display render
+    gl.enableVertexAttribArray(this.displayPosLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.posBuffer);
+    gl.vertexAttribPointer(this.displayPosLocation, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(this.displayTexCoordLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.displayTexcoordBuffer);
+    gl.vertexAttribPointer(this.displayTexCoordLocation, 2, gl.FLOAT, false, 0, 0);
+
+    // Bind textures
+    gl.activeTexture(gl.TEXTURE0);
+    if (this.phase == 0) {
+        gl.bindTexture(gl.TEXTURE_2D, this.focusTextures[1]);
+    } else {
+        gl.bindTexture(gl.TEXTURE_2D, this.focusTextures[0]);
+    }
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, this.srcTexture);
+
+    // draw!
+    gl.drawArrays(primitiveType, offset, count);
+    if (this.phase == 0) {
+        this.phase = 1;
+    } else {
+        this.phase = 0;
+    }
+}
+
+FocusEditor.prototype.createFocusTexture = function(pixelData) {
+    var gl = this.gl;
+    var texture = createAndSetupTexture(gl, 0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, this.focusMap.width, this.focusMap.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, pixelData);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    return texture;
+}
+
+FocusEditor.prototype.createFramebuffer = function(texture) {
+    var gl = this.gl;
+    var framebuffer = gl.createFramebuffer();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+    return framebuffer;
 }
 
 function FocusMap(width, height) {
@@ -65,15 +204,16 @@ function FocusMap(width, height) {
 }
 
 FocusMap.prototype.updatePixels = function() {
-    for (var x = 0; x < this.width; x++) {
-        for (var y = 0; y < this.height; y++) {
+    var c = 0;
+    for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
             var focusValue = this.cells[x][y];
             var pixelValue = Math.floor(255 * focusValue);
-            var c = x * y * 4;
             this.imageData[c] = pixelValue;
             this.imageData[c + 1] = pixelValue;
             this.imageData[c + 2] = pixelValue;
             this.imageData[c + 3] = 255;
+            c += 4;
         }
     }
 }
@@ -81,13 +221,15 @@ FocusMap.prototype.updatePixels = function() {
 FocusMap.prototype.updateFromPixels = function() {
     this.minValue = 1;
     this.maxValue = 0;
-    for (var x = 0; x < this.width; x++) {
-        for (var y = 0; y < this.height; y++) {
-            var pixelValue = this.imageData[x * y * 4];
+    var c = 0;
+    for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
+            var pixelValue = this.imageData[c];
             var focusValue = pixelValue / 255.0;
             this.minValue = Math.min(this.minValue, focusValue);
             this.maxValue = Math.max(this.maxValue, focusValue);
             this.cells[x][y] = focusValue;
+            c += 4;
         }
     }
 }
