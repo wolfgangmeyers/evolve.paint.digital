61293bca50a4bc2209ce9e162e2bd27a6206d97b
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2019-12-10 20:47:31 -0800

    Worker waits for work items, shows connection err

--- evolver-webgl-ts/src/pages/video-jobs/VideoWorkerPage.tsx
+++ evolver-webgl-ts/src/pages/video-jobs/VideoWorkerPage.tsx
@@ -8,6 +8,8 @@ import { brushData, brushes } from "../../engine/brushes";
 import { loadBrushSet } from "../../engine/brushSet";
 import { Config } from "../../engine/config";
 import { Evolver } from "../../engine/evolver";
+import NavbarCollapse from "react-bootstrap/NavbarCollapse";
+import { WorkItem } from "../../server/model";
 
 // TODO: allow the user to set this
 const client = new ServerClient("http://localhost:8081")
@@ -39,34 +41,45 @@ export const VideoWorkerPage: React.FC = () => {
         manualJitter: 0
     }
 
+    const [err, setErr] = React.useState<string>(null);
     const [initialized, setInitialized] = React.useState(false);
     const [lastUpdate, setLastUpdate] = React.useState(new Date().getTime());
     const [config, setConfig] = React.useState(initialConfig);
     const [evolver, setEvolver] = React.useState<Evolver>(null);
+    const [waiting, setWaiting] = React.useState(false);
 
-    
+
     // TODO: refresh page on completion...
 
     const initialize = async () => {
-         const brushSet = await loadBrushSet(brushData, brushes);
-         // TODO: configure which brushes should be on over time
-         for (let tag of brushSet.getTags()) {
-             config.enabledBrushTags[tag] = true;
-         }
+        const brushSet = await loadBrushSet(brushData, brushes);
+        // TODO: configure which brushes should be on over time
+        for (let tag of brushSet.getTags()) {
+            config.enabledBrushTags[tag] = true;
+        }
 
-         setConfig({...config});
-         const evolver = new Evolver(
+        setConfig({ ...config });
+        const evolver = new Evolver(
             document.getElementById("c") as HTMLCanvasElement,
             config,
             brushSet
         );
         setEvolver(evolver);
-        
-        const workItem = await client.getVideoWorkItem();
-        if (!workItem) {
-            alert("No work item returned. Refresh page when work items are available (sorry)");
+
+        let workItem: WorkItem;
+        try {
+            workItem = await client.getVideoWorkItem();
+            while (!workItem) {
+                setWaiting(true);
+                await wait(5000);
+                workItem = await client.getVideoWorkItem();
+            }
+            setWaiting(false);
+        } catch(err) {
+            setErr("Could not contact server at http://localhost:8081");
             return;
         }
+        
 
         const dataURI = "data:image/jpeg;base64," + workItem.imageData;
         const img = new Image();
@@ -101,8 +114,8 @@ export const VideoWorkerPage: React.FC = () => {
 
     React.useEffect(() => {
         if (!initialized) {
-            initialize();
             setInitialized(true);
+            initialize();
         }
         // TODO: poll for progress and completion...
     });
@@ -117,8 +130,19 @@ export const VideoWorkerPage: React.FC = () => {
                         </h4>
                     </CardHeader>
                     <CardBody>
-                        <PaintingEvolverCanvas zoom={true} />
-                        <canvas id="scratch" style={{visibility:"hidden"}} />
+                        {err ? <span className="text-danger">{err}</span> : null}
+                        {waiting ? (
+                            <>
+                                <i className="fa fa-spinner fa-spin"></i>&nbsp;
+                                Waiting for next work item...
+                            </>
+                        ) : (
+                                <>
+                                    <PaintingEvolverCanvas zoom={true} />
+                                    <canvas id="scratch" style={{ visibility: "hidden" }} />
+                                </>
+                            )}
+
                     </CardBody>
                 </Card>
             </div>
